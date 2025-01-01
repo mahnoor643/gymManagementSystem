5b7d2853392b6ff13e163872aad5ce722d53b154
mahnoor643 <mahnoort643@gmail.com>
2025-01-02 02:21:32 +0500

    initial setup

---
+++ Models/User.js
@@ -0,0 +1,28 @@
+const mongoose= require("mongoose");
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required:true,
+            unique:true
+        },
+        email: {
+            type:String,
+            required:true,
+            unique:true
+        },
+        profilePic: {
+            type:String,
+            required:true
+        },
+        password: {
+            type:String,
+            required:true
+        }
+    },
+    {   timestamps:true    }
+);
+
+const User=mongoose.model("User", UserSchema);
+module.exports = User;
---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+const app=express();
+
+//connecting db
+mongoose.connect("mongodb://localhost:27017/gymManagementSystem").then(()=>{
+    console.log("DB connected");
+}).catch((error)=>{
+    console.log("Error occured",error);
+});
+
+//creating endpoints
+
+
+//creating port
+app.listen(200,()=>{
+    console.log("Server is running on port 200");
+});
