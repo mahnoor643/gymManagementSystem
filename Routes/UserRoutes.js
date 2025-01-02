39e5799c69187e969365f50f409eee861f6126dc
mahnoor643 <mahnoort643@gmail.com>
2025-01-03 00:21:01 +0500

    User sign up

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,34 +1,57 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
 const User = require("../Models/User");
 const router = express.Router();
 
 //create user endpoint
-router.post("/signup", async(req,res)=>{
-    try{
-        const {username,email,password}=req.body;
-        //checking if email already exist
-        const alreadyExist=await User.findOne({email});
-        if(alreadyExist){
-            return res.status(409).json({
-                message:"User with this email already exists"
-            })
+router.post(
+    "/signup",
+    [
+        body("username")
+            .notEmpty()
+            .isString()
+            .isLength({ min: 5 })
+            .withMessage("Username is required, atleast 5 characters long"),
+        body("email")
+            .notEmpty()
+            .isEmail()
+            .withMessage("Email should be valid"),
+        body("password")
+            .notEmpty()
+            .isLength({ min: 8 })
+            .withMessage("Password must be 8 characters long")
+    ],
+    async (req, res) => {
+        //Validating input data
+        const error =validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).json({error:error});
         }
-        //making random key pwd
-        const salt = await bcrypt.genSalt(5);
-        const hashedPassword = await bcrypt.hash(password,salt);
-        //Adding data to DB
-        const newUser = new User({
-            username,email,password: hashedPassword
-        });
-        const savedUser = await newUser.save();
-        //returning success response
-        return res.status(201).json(savedUser);
-    }catch(error){
-        //returning error
-        return res.status(500).json({error:error.message});
-    }
-});
+        try {
+            const { username, email, password } = req.body;
+            //checking if email already exist
+            const alreadyExist = await User.findOne({ email });
+            if (alreadyExist) {
+                return res.status(409).json({
+                    message: "User with this email already exists"
+                })
+            }
+            //making random key pwd
+            const salt = await bcrypt.genSalt(5);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            //Adding data to DB
+            const newUser = new User({
+                username, email, password: hashedPassword
+            });
+            const savedUser = await newUser.save();
+            //returning success response
+            return res.status(201).json(savedUser);
+        } catch (error) {
+            //returning error
+            return res.status(500).json({ error: error.message });
+        }
+    });
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const UserRoutes=require("./Routes/UserRoutes");
 
 const app=express();
+app.use(express.json());
 
 //connecting db
 mongoose.connect("mongodb://localhost:27017/rough").then(()=>{
@@ -11,7 +13,12 @@ mongoose.connect("mongodb://localhost:27017/rough").then(()=>{
 });
 
 //creating endpoints
+app.get("/api", (req,res)=>{
+    res.send("endpoint is working perfectly");
+});
 
+//Routes
+app.use("/user",UserRoutes);
 
 //creating port
 app.listen(200,()=>{
